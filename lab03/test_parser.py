7d0dcb49e04021d89e205d0755bc56e1e58cfcc8
egarciaez <166956104+egarciaez@users.noreply.github.com>
2026-02-10 10:09:35 -0600

    Add files via upload

---
+++ lab03/test_parser.py
@@ -0,0 +1,23 @@
+# test_parser.py
+import pytest
+
+# Same import as setup_example.py
+from byte_code.python_311.uabcs_parser import uabcs_parser
+
+
+def test_parse_file_valid_student_grades():
+    parser = uabcs_parser()
+    records = parser.parse_file("student_grades.uabcs")
+
+    assert isinstance(records, list)
+    assert len(records) == 10
+
+    assert records[0]["NAME"] == "Alice"
+    assert records[0]["GRADE"] == 85
+    assert records[0]["PASS"] is True
+
+def test_parse_file_invalid_extension():
+    parser = uabcs_parser()
+
+    with pytest.raises(ValueError):
+        parser.parse_file("student_grades.uabcs")
