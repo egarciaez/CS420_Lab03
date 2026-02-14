9356c4687f1a11c826410ec8ea33aafbb3e682a0
jqchen1299 <107014683+jqchen1299@users.noreply.github.com>
2026-02-13 21:38:58 -0600

    Update test_parser.py

--- lab03/test_parser.py
+++ lab03/test_parser.py
@@ -148,61 +148,13 @@ if __name__ == "__main__":
         test_grade_filter(),
     ]
 
-    print(f"\nTesting")
+    print(f"\nTesting get_records_by_field")
     print(f"--------------")
     print(f"Passed " + str(passed.count(True)) + " tests")
     print(f"Failed " + str(passed.count(False)) + " tests")
     print("")
 
-#### Tests for get_column_names ####
-
-""" Checks if correct column names are returned"""
-
-def test_column_names_correct():
-    
-    try:
-        parser = uabcs_parser()
-        parser.parse_file("student_grades.uabcs")
-
-        expected = ["NAME", "SUBJECT", "GRADE", "PASS", "POINTS"]
-        result = parser.get_column_names()
-
-        assert result == expected
-        return True
-    except Exception:
-        return False
 
 
-
-""" Checks independence of column name """
-
-def test_column_names_independent():
-  
-    try:
-        parser = uabcs_parser()
-        parser.parse_file("student_grades.uabcs")
-
-        cols = parser.get_column_names()
-        cols.append("NEW_COLUMN")
-
-        new_cols = parser.get_column_names()
-
-        assert "NEW_COLUMN" not in new_cols
-        return True
-    except Exception:
-        return False
-
-
-if __name__ == "__main__":
-    passed = [
-        test_column_names_correct(),
-        test_column_names_independent()
-    ]
-
-    print("\nTesting get_column_names()")
-    print("------------------------------")
-    print("Passed " + str(passed.count(True)) + " tests")
-    print("Failed " + str(passed.count(False)) + " tests")
-    print("")
 
 
