9b3736bf66d0d9e93cbe1fede21009f6fd725526
Yarecli <166956341+lunay222@users.noreply.github.com>
2026-02-13 19:09:46 -0600

    Added column tests

--- lab03/test_parser.py
+++ lab03/test_parser.py
@@ -112,3 +112,54 @@ if __name__ == "__main__":
     print("")
 
 
+#### Tests for get_column_names ####
+
+""" Checks if correct column names are returned"""
+
+def test_column_names_correct():
+    
+    try:
+        parser = uabcs_parser()
+        parser.parse_file("student_grades.uabcs")
+
+        expected = ["NAME", "SUBJECT", "GRADE", "PASS", "POINTS"]
+        result = parser.get_column_names()
+
+        assert result == expected
+        return True
+    except Exception:
+        return False
+
+
+
+""" Checks independence of column name """
+
+def test_column_names_independent():
+  
+    try:
+        parser = uabcs_parser()
+        parser.parse_file("student_grades.uabcs")
+
+        cols = parser.get_column_names()
+        cols.append("NEW_COLUMN")
+
+        new_cols = parser.get_column_names()
+
+        assert "NEW_COLUMN" not in new_cols
+        return True
+    except Exception:
+        return False
+
+
+if __name__ == "__main__":
+    passed = [
+        test_column_names_correct(),
+        test_column_names_independent()
+    ]
+
+    print("\nTesting get_column_names()")
+    print("------------------------------")
+    print("Passed " + str(passed.count(True)) + " tests")
+    print("Failed " + str(passed.count(False)) + " tests")
+    print("")
+
